83c6fe1d6507f698c339e9f5123dcfcb68fc1045
matheusgnreis <matheus@e-com.club>
2024-04-24 11:26:15 -0300

    chore: update relevance reorder

--- storefront.webpack.js
+++ storefront.webpack.js
@@ -3,7 +3,8 @@ const path = require('path')
 module.exports = () => ({
   resolve: {
     alias: {
-      './base-config': path.resolve(__dirname, 'template/js/netlify-cms/base-config')
+      './base-config': path.resolve(__dirname, 'template/js/netlify-cms/base-config'),
+      './js/SearchEngine.js': path.resolve(__dirname, 'template/js/custom-js/components/SearchEngine.js'),
     }
   }
 })
